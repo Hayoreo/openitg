1e307b2bf0a78a5a6d2fb8a85673e4c49a18ccad
Jim Nero <nimjero@gmail.com>
2025-07-22 19:30:30 -0700

    Consolidate themes
    Delete arcade and merge home into default. Will do more theme related stuff in the future, but good enough for now.

--- Themes/ITG2/Scripts/Other.lua
+++ Themes/ITG2/Scripts/Other.lua
@@ -6,6 +6,12 @@ function SelectButtonAvailable()
 	return true
 end
 
+-- Hide the timer if "MenuTimer" is disabled
+function HideTimer()
+	local enabled = PREFSMAN:GetPreference("MenuTimer")
+	if enabled then return "0" else return "1" end
+end
+
 function GetWorkoutMenuCommand()
 	GAMESTATE:SetTemporaryEventMode(true)
 	return "difficulty," .. GetInitialDifficulty() .. ";screen,ScreenWorkoutMenu;PlayMode,regular;SetEnv,Workout,1"
--- Themes/arcade/Scripts/Other.lua
+++
@@ -1,6 +0,0 @@
-function Platform() return "arcade" end
-
-function SelectButtonAvailable()
-	if GetInputType() == "PIUIO" then return true end
-	return false
-end
